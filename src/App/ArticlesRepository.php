c3e5459e88e2e7cc1d89e689985b67ef5d09690e
Dysha96 <Dyha752259>
2018-06-30 00:24:25 +0700

    add Repositories

---
+++ src/App/ArticlesRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App;
+
+class ArticlesRepository
+{
+    protected $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function all()
+    {
+        return $this->pdo->query('SELECT * FROM users')->fetchAll();
+    }
+
+    public function articleById($id)
+    {
+        return $this->pdo->query("SELECT * FROM articles WHERE id = $id")->fetch();
+    }
+
+    public function articleTop()
+    {
+        return $this->pdo->query
+        ("SELECT if, title FROM articles ORDER BY views DESC WHERE date>= DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)limit 10")
+            ->fetchAll();
+    }
+
+    public function addArticle($userId, $title, $content, $image = null)
+    {
+        $insert = $this->connection->prepare
+        ("INSERT INTO `articles` SET `image` = :image, `user_id` = :userId, `title` = :title,`content` = :content");
+
+        $result = $insert->execute(['image' => $image, 'userId' => $userId, 'title' => $title, 'content' => $content]);
+
+        return $result;
+    }
+
+    public function updateArticle($userId, $title, $content, $image = null)
+    {
+        if ($image != null) {
+            $insert = $this->connection->prepare
+            ("INSERT INTO `articles` SET `image` = :image, `user_id` = :userId, `title` = :title,`content` = :content");
+            $result = $insert->execute(['image' => $image, 'userId' => $userId, 'title' => $title, 'content' => $content]);
+        } else {
+            $insert = $this->connection->prepare
+            ("INSERT INTO `articles` SET  `user_id` = :userId, `title` = :title,`content` = :content");
+            $result = $insert->execute(['userId' => $userId, 'title' => $title, 'content' => $content]);
+        }
+        return $result;
+    }
+}
+
+
---
+++ src/App/UserRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+class UserRepository
+{
+    protected $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function deleteById($id)
+    {
+        $stmt = $this->pdo->prepare('delete from users where id = ?');
+        return $stmt->execute([$id]);
+    }
+
+    public function all()
+    {
+        return $this->pdo->query('SELECT * FROM users')->fetchAll();
+    }
+
+    public function userByLogin($login)
+    {
+        return $this->pdo->query("SELECT * FROM users WHERE name = $login")->fetch();
+    }
+
+    public function userRoleByLogin($login)
+    {
+        return $this->pdo->query("SELECT role FROM users WHERE name = $login")->fetch();
+    }
+
+    public function addUser($name, $password, $role = null)
+    {
+        $insert = $this->pdo->prepare
+        ("INSERT INTO `Users` SET `name` = :name, `password` = :password, `role` = :role");
+
+        $result = $insert->execute(['name' => $name, 'password' => sha1($password), 'role' => $role]);
+
+        return $result;
+    }
+}
+
+
